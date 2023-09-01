c0a559648742838404acdb1fbe92cf2e70049d9e
Khchimi Othmen <80161433+khchimi-Othmen@users.noreply.github.com>
2023-09-01 14:38:07 +0100

    Enhance transaction processing to support fanout RabbitMQ exchange for broadcasting to multiple microservices.

--- ms-Transaction/src/main/java/tn/stb/mstransaction/config/MessagingConfig.java
+++ ms-Transaction/src/main/java/tn/stb/mstransaction/config/MessagingConfig.java
@@ -5,69 +5,76 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessagingConfig {
 
-    public static final String QUEUE = "first-ms-queue";
-    public static final String EXCHANGE = "first-ms-exchange";
-    public static final String ROUTING_KEY = "first-ms-routing-key";
+    public static final String EX_RATE_QUEUE = "ex-rate-queue";
+    public static final String EX_RATE_EXCHANGE = "ex-rate-exchange";
+    public static final String EX_RATE_ROUTING_KEY = "ex-rate-routing-key";
     ///////////
-    public static final String SECOND_QUEUE = "second-ms-queue";
-    public static final String SECOND_EXCHANGE = "second-ms-exchange";
-    public static final String SECOND_ROUTING_KEY = "second-ms-routing-key";
+
+    public static final String TRS_QUEUE_1 = "transaction-queue-fanout3-ms1";
+    public static final String TRS_QUEUE_2 = "transaction-queue-fanout3-ms2";
+    public static final String TRS_EXCHANGE = "transaction-exchange_fanout3";
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE);
+    public Queue EX_RATE_queue() {
+        return new Queue(EX_RATE_QUEUE,true);
     }
 
     @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
+    public TopicExchange EX_RATE_exchange() {
+        return new TopicExchange(EX_RATE_EXCHANGE);
     }
 
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public Binding EX_RATE_binding(Queue EX_RATE_queue, TopicExchange EX_RATE_exchange) {
+        return BindingBuilder.bind(EX_RATE_queue).to(EX_RATE_exchange).with(EX_RATE_ROUTING_KEY);
+    }
+
+
+    @Bean
+    public Queue trsQueue1() {
+        return new Queue(TRS_QUEUE_1, true);
     }
 
     @Bean
-    public Queue secondQueue() {
-        return new Queue(SECOND_QUEUE);
+    public Queue trsQueue2() {
+        return new Queue(TRS_QUEUE_2, true);
     }
 
     @Bean
-    public TopicExchange secondExchange() {
-        return new TopicExchange(SECOND_EXCHANGE);
+    public FanoutExchange trsExchange() {
+        return new FanoutExchange(TRS_EXCHANGE);
     }
 
     @Bean
-    public Binding secondBinding(Queue secondQueue, TopicExchange secondExchange) {
-        return BindingBuilder.bind(secondQueue).to(secondExchange).with(SECOND_ROUTING_KEY);
+    public Binding trsBinding1(Queue trsQueue1, FanoutExchange trsExchange) {
+        return BindingBuilder.bind(trsQueue1).to(trsExchange);
     }
+
+    @Bean
+    public Binding trsBinding2(Queue trsQueue2, FanoutExchange trsExchange) {
+        return BindingBuilder.bind(trsQueue2).to(trsExchange);
+    }
+
+
 
     @Bean
     public MessageConverter converter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    @Primary
     @Bean
-    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(converter());
         return rabbitTemplate;
     }
-
-
 
 }
